2cfac73e87f37249286df6cb0bcc4c8c31df2670
Oscar Espirilla <oscar.espirilla@laureate.net>
2018-01-03 17:53:57 -0600

    added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     MaterialModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: 'welcome' , component: HomeComponent },
+      { path: '' , redirectTo: 'welcome' , pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome' , pathMatch: 'full' }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
